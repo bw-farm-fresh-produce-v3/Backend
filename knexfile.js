a630f7829e08b3b839b08d0825662019fc568823
Moses Samuel <mosamuel98@gmail.com>
2019-12-25 11:55:07 -0500

    reconfigure knexfile again

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,9 @@
-// Update with your config settings.
-
 module.exports = {
   development: {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './data/auth.db3',
+      filename: './data/dev.db3',
     },
     pool: {
       afterCreate: (conn, done) => {
@@ -20,32 +18,33 @@ module.exports = {
     },
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   test: {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './data/auth.db3',
+      filename: './data/test.db3',
     },
     pool: {
       afterCreate: (conn, done) => {
         conn.run('PRAGMA foreign_keys = ON', done);
       },
+    },
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+
+
+  staging: {
+    client: 'postgresql',
+    // TODO: Change this URL to that of deployed db
+    connection: "postgres://localhost/webauth-iii-challenge",
+    pool: {
+      min: 2,
+      max: 10
     },
     migrations: {
       directory: './data/migrations',
@@ -57,17 +56,17 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    // TODO: Change this URL to that of deployed db
+    connection: "postgres://localhost/webauth-iii-challenge",
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
   }
 };
