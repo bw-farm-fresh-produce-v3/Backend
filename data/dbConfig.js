672f4c8e22f16bf51c8a12e96aa6e37223cf9374
Moses Samuel <mosamuel98@gmail.com>
2020-01-06 11:59:58 -0500

    export different database configurations based on the environment

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -2,4 +2,17 @@ const knex = require('knex');
 
 const knexConfig = require('../knexfile.js');
 
-module.exports = knex(knexConfig.development);
+switch (process.env.NODE_ENV.toUpperCase()) {
+    case "DEVELOPMENT": 
+        module.exports = knex(knexConfig.development)
+        break
+    case "PRODUCTION":
+        module.exports = knex(knexConfig.production)
+        break
+    case "TEST":
+        module.exports = knex(knexConfig.test)
+        break
+    default: 
+        console.warn("Ensure that your NODE_ENV is specified. Its current value is: ", process.env.NODE_ENV)
+        module.exports = knex(knexConfig.development)
+}
