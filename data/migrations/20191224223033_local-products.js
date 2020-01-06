d6c9de56b60c9ddbaeb08e5b8e4e4215382ace4c
Moses Samuel <mosamuel98@gmail.com>
2020-01-06 14:38:50 -0500

    delete all migrations in support of new schema design

--- data/migrations/20191224223033_local-products.js
+++
@@ -1,8 +0,0 @@
-
-exports.up = function(knex) {
-    /** UNNECESSARY cause a tomato can only be in one place at a time.  */
-};
-
-exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('local-products')
-};
